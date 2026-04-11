fd80319241908af3a4e68386e02788580d4ac6a4
Tejas Sharma <tejas.sharma.1729@gmail.com>
2026-04-11 16:00:08 +0530

    Added Graph Attention Layer

---
+++ models/attention.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+
+from typing import List, Tuple, Dict, Set, Iterable, Callable, Literal, Optional, Any, Union
+
+import torch_geometric
+import torch
+from torch import nn, tensor, Tensor, autograd, optim, cuda, mps, cpu, distributions
+from torch.nn import Module, Parameter, ModuleList, ModuleDict, functional as F
+from torch.optim import Optimizer, Adam, AdamW, SGD, RMSprop, lr_scheduler
+
+import torch_geometric
+from torch_geometric.nn import MessagePassing, GCNConv, SAGEConv, GATConv, GINConv, GIN
+from torch_geometric.data import Data, DataLoader, Dataset, InMemoryDataset
+from torch_geometric.utils import add_self_loops, degree, to_dense_adj, to_dense_batch, coalesce
+
+
+"""
+Graph attention layer.
+
+This computes the attention of matrices similar to attention in transformer networks.
+The twist is that attention is zero for nodes not reachable from each other, and
+there is an option to have weighted attention based on distance (number of hops) between nodes, 
+but those distance attention factors are fixed during initialization.
+"""
+class AttentionLayer(Module):
+    def __init__(
+            self,
+            embed_dim: int,
+            num_heads: int,
+            head_dim: int,
+            attn_distance_factors: List[float] | None,
+            device: torch.device
+    ) -> None:
+        """
+        Initialize the attention layer.
+
+        Args:
+            embed_dim: The embedding dimension
+            num_heads: The number of attention heads
+            head_dim: The dimension of each attention head
+            attn_distance_factors: The attention distance factors (hyperparameters for weighing attention)
+            device: The device to run the layer on
+        """
+        self.embed_dim: int = embed_dim # dimension of the input and output embeddings
+        self.num_heads: int = num_heads # number of attention heads
+        self.head_dim: int = head_dim # dimension of each attention head
+        self.attn_distance_factors: List[float] | None = attn_distance_factors # for weighted attention, preferential to neighbors
+        self.query_weights = nn.Linear(embed_dim, num_heads * head_dim) # linear transformation for query embeddings
+        self.key_weights = nn.Linear(embed_dim, num_heads * head_dim)   # linear transformation for key embeddings
+        self.value_weights = nn.Linear(embed_dim, num_heads * head_dim) # linear transformation for value embeddings
+        self.update_weights = nn.Linear(num_heads * head_dim, embed_dim) # linear transformation for output embeddings
+        self.device = device # device to run the layer on
+        self.to(device) # move the layer to the device
+    
+    def forward(
+            self,
+            input_graphs: Data | List[Data],
+            input_embeddings: Tensor,
+    ) -> Tensor:
+        """
+        Forward pass for a batch of graphs or a single graph.
+
+        This computes the Q, K and V embeddings of the input embeddings as they are.
+
+        For the attention, it computes a dynamic attention mask, which
+        - if there are no distance factors, it is just the reachability matrix (1 or 0)
+        - if there are distance factors, the mask is a floating matrix, based on the distance between the nodes
+        - between 2 non-reachable nodes or nodes in different graphs, the attention mask is zero
+
+        With the mask, it uses flash attention and also computes the attention projection.
+        
+        Args:
+            input_graphs: The input graphs (or a single graph)
+            input_embeddings [net_num_vertices, embed_dim]: The input embeddings for all vertices of all graphs, in order.
+        
+        Returns:
+            The output embeddings.
+        """
+        single_graph: bool = isinstance(input_graphs, Data) or not isinstance(input_graphs, list)
+        if single_graph:
+            assert isinstance(input_graphs, Data)
+            input_graphs = [input_graphs]
+        
+        assert isinstance(input_graphs, list)
+        # One per graph: number of vertices (in order)
+        num_vertices: List[int] = []
+        for graph in input_graphs:
+            assert graph.num_nodes is not None
+            num_vertices.append(graph.num_nodes)
+        net_num_vertices: int = sum(num_vertices).__int__()
+        assert input_embeddings.shape == torch.Size([net_num_vertices, self.embed_dim])
+        
+        # Base attention mask = edge matrix.
+        base_attn_mask: Tensor = torch.zeros((net_num_vertices, net_num_vertices))
+        prev_num_vertices: int = 0
+        for i, graph in enumerate(input_graphs):
+            assert graph.edge_index is not None
+            assert graph.edge_index.shape == torch.Size([2, graph.edge_index.shape[1]])
+            prev_num_vertices += num_vertices[i]
+            for (start, end) in zip(graph.edge_index[0], graph.edge_index[1]):
+                base_attn_mask[start + prev_num_vertices, end + prev_num_vertices] = 1.0
+        
+        # The mask that is acutally used for attention.
+        attn_factors: Tensor = torch.eye(net_num_vertices)
+        if self.attn_distance_factors is None:
+            while True:
+                # Since discrete mask, it will converge in finite steps
+                attn_factors_new: Tensor = attn_factors @ base_attn_mask
+                if torch.allclose(attn_factors_new, attn_factors):
+                    # Stop when fixed point reachability converges
+                    break
+                attn_factors = attn_factors_new
+        else:
+            assert self.attn_distance_factors is not None
+            attn_mask: Tensor = base_attn_mask
+            for i, factor in enumerate(self.attn_distance_factors):
+                attn_mask = attn_mask @ base_attn_mask
+                attn_factors = torch.max(attn_factors, factor * attn_mask)
+        
+        # The main (GPU-heavy) computation
+        query_embeddings: Tensor = self.query_weights(input_embeddings)
+        key_embeddings: Tensor = self.key_weights(input_embeddings)
+        value_embeddings: Tensor = self.value_weights(input_embeddings)
+
+        # GPU work: flash attention with floating attention mask (log for proper float masking)
+        attn_output: Tensor = F.scaled_dot_product_attention(
+            query_embeddings,
+            key_embeddings,
+            value_embeddings,
+            attn_mask=attn_factors.log(), # Log for proper float masking.
+            dropout_p=0.0,
+            is_causal=False,
+        )
+
+        # Finally, compute the output embeddings with a linear transformation.
+        out_embeddings: Tensor = self.update_weights(attn_output)
+        assert out_embeddings.shape == torch.Size([net_num_vertices, self.embed_dim])
+        return out_embeddings
--- models/gnn.py
+++ models/gnn.py
@@ -13,6 +13,18 @@ from torch_geometric.nn import MessagePassing, GCNConv, SAGEConv, GATConv, GINCo
 from torch_geometric.data import Data, DataLoader, Dataset, InMemoryDataset
 from torch_geometric.utils import add_self_loops, degree, to_dense_adj, to_dense_batch, coalesce
 
+
+"""
+A single layer in the GNN consists of the following steps:
+1. For each vertex, compute a node embedding by applying a linear transformation to the input embedding
+2. For each edge, compute an edge embedding by applying a linear transformation to the concatenation of the 
+        node embeddings of the two vertices connected by the edge
+3. For each vertex, compute a pre-GELU embedding by concatenating the node embedding of the vertex, 
+        the sum of the node embeddings of the neighboring vertices, 
+        and the sum of the edge embeddings of the edges connected to the vertex
+4. For each vertex, compute a post-GELU embedding by applying the GELU activation function
+5. For each vertex, compute the output embedding by applying a linear transformation to the post-GELU embedding
+"""
 class GNNLayer(Module):
     def __init__(
         self,
@@ -21,78 +33,94 @@ class GNNLayer(Module):
     ) -> None:
         """
         Initialize the GNN layer.
+
+        Args:
+            embed_dim: The embedding dimension.
+            device: The device to run the layer on.
         """
-        self.embed_dim: int = embed_dim
-        self.node_weights = nn.Linear(embed_dim, embed_dim)
-        self.edge_weights = nn.Linear(2 * embed_dim, embed_dim)
-        self.embed_gelu = nn.GELU()
-        self.embed_update = nn.Linear(3 * embed_dim, embed_dim)
-        self.device = device
-        self.to(device)
+        self.embed_dim: int = embed_dim # dimension of the input and output embeddings
+        self.node_weights = nn.Linear(embed_dim, embed_dim) # linear transformation for node embeddings
+        self.edge_weights = nn.Linear(2 * embed_dim, embed_dim) # linear transformation for edge embeddings 
+        self.embed_gelu = nn.GELU() # GELU activation function
+        self.embed_update = nn.Linear(3 * embed_dim, embed_dim) # linear transformation for output embeddings
+        self.device = device # device to run the layer on
+        self.to(device) # move the layer to the device
     
     def forward(
         self,
-        input_graph: Data,
+        input_graphs: Data | List[Data],
         input_embeddings: Tensor,
     ) -> Tensor:
         """
-        Forward pass for a single graph.
-        """
-        num_vertices: int = input_graph.num_nodes
-        assert input_embeddings.shape == torch.Size(num_vertices, self.embed_dim)
+        Forward pass for a batch of graphs or a single graph.
 
-        node_embeddings = self.node_weights(input_embeddings)
-        edge_embeddings = self.edge_weights(torch.cat(
-            [input_embeddings[input_graph.edge_index[0]],
-             input_embeddings[input_graph.edge_index[1]]
-        ], dim=1))
-        
-        pre_gelu_embeddings = torch.zeros((num_vertices, 3 * self.embed_dim))
-        for i in range(num_vertices):
-            pre_gelu_embeddings[i, :self.embed_dim] = node_embeddings[i]
-        for (start, end) in zip(input_graph.edge_index[0], input_graph.edge_index[1]):
-            pre_gelu_embeddings[start, self.embed_dim:2*self.embed_dim] += node_embeddings[end]
-            pre_gelu_embeddings[start, 2*self.embed_dim:3*self.embed_dim] += edge_embeddings[start, end]
-        
-        gelu_embeddings = self.embed_gelu(pre_gelu_embeddings)
-        out_embeddings = self.embed_update(gelu_embeddings)
-        assert out_embeddings.shape == torch.Size(num_vertices, self.embed_dim)
-        return out_embeddings
-    
-    def forward(
-        self,
-        input_graphs: List[Data],
-        input_embeddings: Tensor,
-    ) -> Tensor:
-        """
-        Forward pass for a batch of graphs.
-        """
-        num_vertices: List[int] = [graph.num_nodes for graph in input_graphs]
-        assert input_embeddings.shape == torch.Size(sum(num_vertices).__int__(), self.embed_dim)
+        This function computes the output embeddings for all vertices of all graphs in the batch, in order.
+        It first computes the node embeddings and edge embeddings, and then computes the pre-GELU embeddings 
+        per vertex by concatenating its node embedding, edge embedding and the sum of the neighbors' node embeddings.
+        A GELU transformation followed by a final linear transformation gives the output embeddings.
 
-        node_embeddings = self.node_weights(input_embeddings)
-        vertices_cumsum: List[int] = [0]
-        for num_vertex in num_vertices:
-            vertices_cumsum.append(vertices_cumsum[-1] + num_vertex)
-        edge_indices = torch.cat([graph.edge_index + vertices_cumsum[i] for i, graph in enumerate(input_graphs)])
-        edge_embeddings = self.edge_weights(torch.cat(
+        Args:
+            input_graphs: The input graphs (or a single graph)
+            input_embeddings [net_num_vertices, embed_dim]: The input embeddings for all vertices of all graphs, in order.
+        
+        Returns:
+            The output embeddings.
+        """
+        single_graph: bool = isinstance(input_graphs, Data) or not isinstance(input_graphs, list)
+        if single_graph:
+            assert isinstance(input_graphs, Data)
+            input_graphs = [input_graphs]
+        
+        assert isinstance(input_graphs, list)
+        # One per graph: number of vertices (in order)
+        num_vertices: List[int] = []
+        for graph in input_graphs:
+            assert graph.num_nodes is not None
+            num_vertices.append(graph.num_nodes)
+        assert input_embeddings.shape == torch.Size([sum(num_vertices).__int__(), self.embed_dim])
+
+        # Compute edge embeddings
+        net_num_vertices: int = 0
+        edge_indices_list: List[Tensor] = []
+        for i, graph in enumerate(input_graphs):
+            assert graph.edge_index is not None
+            assert graph.edge_index.shape == torch.Size([2, graph.edge_index.shape[1]])
+            net_num_vertices += num_vertices[i]
+            edge_indices_list.append(graph.edge_index + net_num_vertices)
+        edge_indices = torch.cat(edge_indices_list)
+
+        # GPU work: compute node and edge embeddings
+        node_embeddings: Tensor = self.node_weights(input_embeddings)
+        edge_embeddings: Tensor = self.edge_weights(torch.cat(
             [node_embeddings[edge_indices[0]],
              node_embeddings[edge_indices[1]]
         ], dim=1))
 
-        pre_gelu_embeddings = torch.zeros((vertices_cumsum[-1], 3 * self.embed_dim))
-        for i in range(num_vertices):
+        # Move embeddings to pre-GELU embeddings 
+        # (concatenation of node embedding, sum of neighbors' node embeddings and sum of edge embeddings)
+        pre_gelu_embeddings: Tensor = torch.zeros((net_num_vertices, 3 * self.embed_dim))
+        for i in range(num_vertices.__len__()):
             pre_gelu_embeddings[i, :self.embed_dim] = node_embeddings[i]
         for (start, end) in zip(edge_indices[0], edge_indices[1]):
             pre_gelu_embeddings[start, self.embed_dim:2*self.embed_dim] += node_embeddings[end]
             pre_gelu_embeddings[start, 2*self.embed_dim:3*self.embed_dim] += edge_embeddings[start, end]
         
-        gelu_embeddings = self.embed_gelu(pre_gelu_embeddings)
-        out_embeddings = self.embed_update(gelu_embeddings)
-        assert out_embeddings.shape == torch.Size(vertices_cumsum[-1], self.embed_dim)
+        # GPU work: GELU --> output embeddings
+        gelu_embeddings: Tensor = self.embed_gelu(pre_gelu_embeddings)
+        out_embeddings: Tensor = self.embed_update(gelu_embeddings)
+        assert out_embeddings.shape == torch.Size([net_num_vertices, self.embed_dim])
         return out_embeddings
         
         
+"""
+The full GNN consists of multiple layers of type GNNLayer.
+
+Each GNN layer compute the node embeddings, edge embeddings and concatenates them with the
+sum of neighbors' node embeddings (per vertex) before applying the GELU activation function.
+A final linear transformation is applied to the post-GELU embeddings to get the output embeddings.
+
+This is repeated for each layer in the GNN.
+"""
 class GNN(Module):
     def __init__(
         self,
@@ -103,27 +131,17 @@ class GNN(Module):
         """
         Initialize the Graph Neural Network
         """
-        self.embed_dim: int = embed_dim
-        self.num_layers: int = num_layers
-        self.layers = ModuleList([GNNLayer for layer in range(num_layers)])
-        self.to(device)
+        self.embed_dim: int = embed_dim # dimension of the input and output embeddings
+        self.num_layers: int = num_layers # number of layers in the GNN
+        self.layers = ModuleList([
+            GNNLayer(embed_dim, device) for layer in range(num_layers)
+        ]) # list of GNN layers
+        self.device = device # device to run the GNN on
+        self.to(device) # move the GNN to the device
     
     def forward(
         self,
-        input_graph: Data,
-        input_embeddings: Tensor,
-    ) -> Tensor:
-        """
-        Forward pass for a single graph.
-        """
-        out_embeddings = input_embeddings
-        for layer in self.layers:
-            out_embeddings = layer(input_graph, out_embeddings)
-        return out_embeddings
-    
-    def forward(
-        self,
-        input_graphs: List[Data],
+        input_graphs: Data | List[Data],
         input_embeddings: Tensor,
     ) -> Tensor:
         """
@@ -131,5 +149,5 @@ class GNN(Module):
         """
         out_embeddings = input_embeddings
         for layer in self.layers:
-            out_embeddings = layer(input_graph, out_embeddings)
+            out_embeddings = layer(input_graphs, out_embeddings)
         return out_embeddings
